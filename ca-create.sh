56b5b0923397cb34a2110d2c7726a3e86cc4e0d1
Theodore A. Roth <troth@openavr.org>
2026-07-15 16:44:22 -0600

    Refactor to generate multiple key and ca types
    Generates multiple keys for user and host signing operation and import
    them it a yubikey.
    
    OpenSSH >= 10.3p1 can now do all of the convertions to formats needed by
    the ykman tool without needing to use openssl commands.
    
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

--- ca-create.sh
+++ ca-create.sh
@@ -1,44 +1,94 @@
 #!/bin/bash
+#
+# Script to create a CA private keys that can be used for signing openssh
+# certificates while storing the private key ina Yubikey PIV slot.
+# Use ED25519 and ECDSA key types for user and host CA keys.
+#
 
-OPENSSL_KEY="keys/id_ec384_user_ca_key.pem"
-OPENSSL_PUB="keys/id_ec384_user_ca_pub.pem"
-OPENSSH_PUB="keys/id_ec384_user_ca.pub"
-# OPENSSL_CRT="keys/id_ec384_user_ca_piv-cert.pem"
-SLOT="9a"
+KEY_TYPES=(
+    ed25519
+    ecdsa
+)
 
-SUBJECT="CN=SSH USER CA"
-TTL_DAYS="30"
+CA_TYPES=(
+    user
+    host
+)
+
+declare -A SLOTS=(
+    [user-ed25519]="82"
+    [user-ecdsa]="83"
+    [host-ed25519]="84"
+    [host-ecdsa]="85"
+)
+
+TTL_DAYS="90"
 
 DEF_PIN="123456"
 DEF_PUK="12345678"
 DEF_MGMT="010203040506070801020304050607080102030405060708"
 
-mkdir -p keys
+function create_ca_key_ed25519()
+{
+    local KEY="${1}"
+    ssh-keygen -t ed25519 -N '' -m PKCS8 -f ${KEY}
+}
+
+function create_ca_key_ecdsa()
+{
+    local KEY="${1}"
+    ssh-keygen -t ecdsa -b 384 -N '' -m PKCS8 -f ${KEY}
+}
+
+function init_key()
+{
+    local CT=$1
+    local KT=$2
+
+    local OPENSSH_KEY="keys-ca/id_${KT}_${CT}_ca"
+    local OPENSSH_PUB="keys-ca/id_${KT}_${CT}_ca.pub"
+    local OPENSSL_PUB="keys-ca/id_${KT}_${CT}_ca_pub.pem"
+
+    local SUBJECT="CN=SSH ${CT} CA"
+    local SLOT="${SLOTS[${CT}-${KT}]}"
+
+    if [ ! -f ${OPENSSH_KEY} ]; then
+        create_ca_key_${KT} "${OPENSSH_KEY}" || exit 1
+    fi
+
+    # Import the private key into the yubikey.
+    ykman piv keys import \
+        -m "${DEF_MGMT}" \
+        ${SLOT} ${OPENSSH_KEY}
+
+    # Extract the public key from the yubikey.
+    ykman piv keys export \
+        ${SLOT} ${OPENSSL_PUB}
+
+    # Convert openssl pub to openssh pub format.
+    #ssh-keygen -i -m PKCS8 -f ${OPENSSL_PUB} > ${OPENSSH_PUB}
+
+    # Create a self-signed cert in the yubikey.
+    # This cert is not used by ssh tools, is only needed by the PIV
+    # application in the yubikey.
+    ykman piv certificates generate \
+        -P "${DEF_PIN}" \
+        -m "${DEF_MGMT}" \
+        -s "${SUBJECT}" \
+        -d ${TTL_DAYS} \
+        ${SLOT} ${OPENSSL_PUB}
+}
 
 set -x
 
-ykman piv reset || exit 1
+mkdir -p keys-ca
 
-# Generate the key pair
-if [ ! -f ${OPENSSL_KEY} ]; then
-    openssl ecparam -genkey -name secp384r1 -noout -out ${OPENSSL_KEY}
-fi
-
-ykman piv keys import \
-    -m "${DEF_MGMT}" \
-    ${SLOT} ${OPENSSL_KEY}
-
-ykman piv keys export \
-    ${SLOT} ${OPENSSL_PUB}
-
-ykman piv certificates generate \
-    -P "${DEF_PIN}" \
-    -m "${DEF_MGMT}" \
-    -s "${SUBJECT}" \
-    -d ${TTL_DAYS} \
-    ${SLOT} ${OPENSSL_PUB}
+for CT in "${CA_TYPES[@]}"
+do
+    for KT in "${KEY_TYPES[@]}"
+    do
+        init_key "${CT}" "${KT}"
+    done
+done
 
 ykman piv info
-
-# Convert openssl pub to openssh pub format
-ssh-keygen -i -m PKCS8 -f ${OPENSSL_PUB} > ${OPENSSH_PUB}
--- ca-sign-user.sh
+++ ca-sign-user.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
+CA_PUB="${1:-keys-ca/id_ed25519_user_ca.pub}"
+
 PKCS11="/usr/lib/x86_64-linux-gnu/libykcs11.so"
-KEY="keys/id_ed25519_${USER}"
-PUB="${KEY}.pub"
+USER_KEY="keys-user/id_ed25519_${USER}"
+USER_PUB="${USER_KEY}.pub"
 
-CA_PUB="keys/id_ec384_user_ca.pub"
+mkdir -p keys-user
 
-mkdir -p keys
-
-ssh-keygen -t ed25519 -f ${KEY}
+ssh-keygen -t ed25519 -f ${USER_KEY}
 
 ssh-keygen \
     -s "${CA_PUB}" \
     -D "${PKCS11}" \
     -I foo \
     -V '-4d:+8d' \
-    -n ${USER} "${PUB}"
+    -n ${USER} "${USER_PUB}"
