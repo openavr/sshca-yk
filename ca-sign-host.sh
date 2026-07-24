bf09796d0f546745c91ab111077c60cfc732c24a
Theodore A. Roth <troth@openavr.org>
2026-07-24 17:41:54 -0600

    Add host key signing script
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

---
+++ ca-sign-host.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+HOST="${1}"
+DOMAIN="${2}"
+HOST_PUB="${3}"
+
+if [ -z "${HOST}" ] || [ -z "${DOMAIN}" ] || [ -z "${HOST_PUB}" ]
+then
+    echo "Usage: $0 <hostname> <domain> <server-pub-key>"
+    exit 1
+fi
+
+CA_PUB="${CA_PUB:-keys-ca/${DOMAIN}/id_ed25519_host_ca.pub}"
+
+PKCS11="/usr/lib/x86_64-linux-gnu/libykcs11.so"
+
+SIGN_OPTS=(
+    -h
+    -s "${CA_PUB}"
+    -D "${PKCS11}"
+    -I "${HOST}.${DOMAIN}"
+    -n "${HOST}.${DOMAIN},${HOST}"
+    -V '+52w'
+
+    -O no-user-rc
+    # -O no-pty
+    # -O no-agent-forwarding
+    # -O no-port-forwarding
+    # -O no-x11-forwarding
+    # -O force-command='/usr/bin/uptime'
+    "${HOST_PUB}"
+)
+
+set -x
+
+# Sign a certificate for the new key
+ssh-keygen "${SIGN_OPTS[@]}"
