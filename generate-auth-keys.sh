1ce7f5e5cac750edcd035f41d90bc8ce4ab67a53
Theodore A. Roth <troth@openavr.org>
2026-07-24 16:27:56 -0600

    Rework generate-auth-keys.sh
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

--- generate-auth-keys.sh
+++ generate-auth-keys.sh
@@ -7,16 +7,43 @@ PUB_KEYS=(
     keys-ca/id_ed25519_host_ca.pub
 )
 
-cat "${PUB_KEYS[@]}" | awk '{print $1 " " $2}'> authorized_keys_ca
+declare -A CA_KEYS
+declare -A CA_COMMENTS
 
-ssh-keygen -D /usr/lib/x86_64-linux-gnu/libykcs11.so -e | grep Retired | awk '{print $1 " " $2}' > authorized_keys_yk
+function process_ca_key() {
+    local FN="$1"
+    local KT="$2"
+    local KEY="$3"
+    shift 3
+    local COMMENT="$*"
 
-set -x
-if ! diff -u authorized_keys_ca authorized_keys_yk
-then
-    echo "ERROR: pub keys don't match those in yubikey"
-    exit 1
-fi
+    CA_KEYS["${KEY}"]="${FN}"
+    CA_COMMENTS["${KEY}"]="${COMMENT}"
+}
 
-sed -i -e 's/^/cert-authority /' authorized_keys_ca
-cat authorized_keys_ca
+for key_file in "${PUB_KEYS[@]}"
+do
+    while read -a key_info
+    do
+        process_ca_key "${key_file}" "${key_info[@]}"
+    done < "${key_file}"
+done
+
+function process_yk_key() {
+
+    local KEY_TYPE="${1}"
+    local KEY="${2}"
+    shift 2
+    local COMMENT="${*}"
+
+    local FN="${CA_KEYS[${KEY}]}"
+    if [ -n "${FN}" ]; then
+        echo "'${FN}' is in Yubikey (${COMMENT}: ${CA_COMMENTS[${KEY}]}): ${KEY_TYPE}" 1>&2
+        echo "cert-authority ${KEY_TYPE} ${KEY} ${CA_COMMENTS[${KEY}]}"
+    fi
+}
+
+while read -a key_info
+do
+    process_yk_key "${key_info[@]}"
+done < <(ssh-keygen -D /usr/lib/x86_64-linux-gnu/libykcs11.so) >authorized_keys_ca
