2535385e5be38643f5d473d96910ccbce121ddde
Theodore A. Roth <troth@openavr.org>
2026-08-28 17:09:47 -0600

    yk-utils.sh: Avoid running 'ykman list' twice
    Running 'ykman list' is slow, so avoid doing it twice.
    
    Signed-off-by: Theodore A. Roth <troth@openavr.org>

--- yk-utils.sh
+++ yk-utils.sh
@@ -28,32 +28,31 @@ function ykman_cmd() {
 function yk_select() {
     declare -A YK_DEVICES
 
-    # Serial number provided in environment, verify that the Yubikey with
-    # that serial number is actually plugged in to the system.
-    if [[ -n ${YK_SERNUM} ]]
-    then
-        if ${YKMAN} list | grep -e "${YK_SERNUM}"
+    while read -a ykdevice
+    do
+        # Serial number provided in environment, verify that the Yubikey with
+        # that serial number is actually plugged in to the system.
+        if [[ -n ${YK_SERNUM} ]] && [[ ${YK_SERNUM} == ${ykdevice[-1]} ]]
         then
             printf -v YK_CFG "${YK_CFG_FMT}" "${YK_SERNUM}"
+            export YK_SERNUM
+            export YK_CFG
             return 0
         fi
-        echo "WARN: YK_SERNUM in env is invalid: Ignoring it: ${YK_SERNUM}"
-    fi
+
+        YK_DEVICES["${ykdevice[*]}"]="${ykdevice[-1]}"
+    done < <(${YKMAN} list)
 
     unset YK_SERNUM
     declare -g YK_SERNUM
-
-    while read -a ykdevice
-    do
-        YK_DEVICES["${ykdevice[*]}"]="${ykdevice[-1]}"
-    done < <(${YKMAN} list)
 
     if [[ ${#YK_DEVICES[@]} -eq 0 ]]; then
         echo "Please insert a Yubikey, none found" 1>&2
         return 1
     elif [[ ${#YK_DEVICES[@]} -eq 1 ]]; then
-        YK_SERNUM="${YK_DEVICES[@]}"
+        export YK_SERNUM="${YK_DEVICES[@]}"
         printf -v YK_CFG "${YK_CFG_FMT}" "${YK_SERNUM}"
+        export YK_CFG
         return 0
     fi
 
@@ -71,8 +70,9 @@ function yk_select() {
                         return 2
                         ;;
                     *)
-                        YK_SERNUM="${YK_DEVICES[${ykdev}]}"
+                        export YK_SERNUM="${YK_DEVICES[${ykdev}]}"
                         printf -v YK_CFG "${YK_CFG_FMT}" "${YK_SERNUM}"
+                        export YK_CFG
                         return 0
                         ;;
                 esac
